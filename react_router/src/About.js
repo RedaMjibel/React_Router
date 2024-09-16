4613157df486df54709eec5b77f5331f5520582d
Reda Mjibel <reda_mjibel@hotmail.com>
2024-09-17 00:06:21 +0100

    header about and footer components

---
+++ react_router/src/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <main className="About">
+            <h2>About</h2>
+            <p style={{marginTop: "1rem" }}>
+                This blog app is a project with the aim of learning react.
+            </p>
+        </main>
+    )
+}
+
+export default About
